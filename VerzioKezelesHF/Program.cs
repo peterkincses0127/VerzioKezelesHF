86a81828ba81ca6a1c3deac5e0f688f813f18d8f
Petrik Lajos Szg <info@petrik.hu>
2020-09-21 14:03:26 +0200

    Add project files.

---
+++ VerzioKezelesHF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg 10 db számot: ");
+            List<int> numbersArray = new List<int>();
+            for (int i = 0; i < numbersArray.Count; i++)
+            {
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Ennek az átlaga: ");
+        }
+    }
+}
