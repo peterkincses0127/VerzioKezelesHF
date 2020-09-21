bfb8c11f9d4283cf96a9064b9bb2297ce0110b81
Petrik Lajos Szg <info@petrik.hu>
2020-09-21 14:03:43 +0200

    Létrehoztam a projektet

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -7,15 +7,7 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-            Console.Write("Adjon meg 10 db számot: ");
-            List<int> numbersArray = new List<int>();
-            for (int i = 0; i < numbersArray.Count; i++)
-            {
-                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
-
-            }
-
-            Console.WriteLine("Ennek az átlaga: ");
+            
         }
     }
 }
