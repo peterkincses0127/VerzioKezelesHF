58429e416ef5f129c7673e5d83e0653386cc918c
Petrik Lajos Szg <info@petrik.hu>
2020-09-21 14:06:00 +0200

    Feltöltöttem a tömböt számokkal és kiszámoltam az áltagukat

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -7,7 +7,21 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Adjon meg 10 db számot: ");
+            List<int> numbersArray = new List<int>();
+            for (int i = 0; i < numbersArray.Count; i++)
+            {
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int temp = 0;
+            for (int i = 0; i < numbersArray.Count; i++)
+            {
+                temp += numbersArray[i];
+
+            }
+
+            Console.WriteLine("Ennek az átlaga: "+ temp / numbersArray.Count+1);
         }
     }
 }
